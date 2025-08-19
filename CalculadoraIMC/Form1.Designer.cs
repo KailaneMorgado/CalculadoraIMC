cd167d4275e77adf96e021ba06a0853bfb2e7465
Kailane Morgado <kaaholiveira2504@gmail.com>
2025-08-18 22:16:48 -0300

    Calculadora de IMC em C#
    Este projeto apresenta uma calculadora de Índice de Massa Corporal desenvolvida com sistema robusto de validação. A aplicação implementa tratamento completo de erros, impedindo a inserção de caracteres não numéricos e alertando sobre campos vazios. O sistema calcula automaticamente o IMC a partir dos dados de peso e altura inseridos, categorizando os resultados em faixas específicas como baixo peso, peso normal, sobrepeso e obesidade. A interface foi projetada com foco na experiência do usuário, apresentando design limpo e responsivo. O projeto demonstra a aplicação prática de validação de formulários, cálculos matemáticos e lógica condicional para interpretação de resultados de saúde.

--- CalculadoraIMC/Form1.Designer.cs
+++ CalculadoraIMC/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CalculadoraIMC
 {
-    partial class Form1
+    partial class frmNome
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmNome));
+            this.lblIMC = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.lblSaude = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblIMC
+            // 
+            this.lblIMC.AutoSize = true;
+            this.lblIMC.Font = new System.Drawing.Font("Britannic Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIMC.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(245)))), ((int)(((byte)(230)))));
+            this.lblIMC.Location = new System.Drawing.Point(169, 35);
+            this.lblIMC.Name = "lblIMC";
+            this.lblIMC.Size = new System.Drawing.Size(209, 30);
+            this.lblIMC.TabIndex = 0;
+            this.lblIMC.Text = "Calcule Seu IMC:";
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(245)))), ((int)(((byte)(230)))));
+            this.lblPeso.Location = new System.Drawing.Point(56, 97);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(145, 24);
+            this.lblPeso.TabIndex = 1;
+            this.lblPeso.Text = "Digite seu peso:";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(221, 101);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(100, 20);
+            this.txtPeso.TabIndex = 2;
+            this.txtPeso.TextChanged += new System.EventHandler(this.txtPeso_TextChanged);
+            this.txtPeso.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtPeso_KeyDown);
+            this.txtPeso.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPeso_KeyPress);
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(245)))), ((int)(((byte)(230)))));
+            this.lblAltura.Location = new System.Drawing.Point(54, 151);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(147, 24);
+            this.lblAltura.TabIndex = 3;
+            this.lblAltura.Text = "Digite sua altura:";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(221, 151);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 4;
+            this.txtAltura.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAltura_KeyDown);
+            this.txtAltura.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAltura_KeyPress);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(234, 190);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(245)))), ((int)(((byte)(230)))));
+            this.lblResultado.Location = new System.Drawing.Point(91, 324);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(53, 24);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = "IMC:";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(221, 329);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 7;
+            // 
+            // lblSaude
+            // 
+            this.lblSaude.AutoSize = true;
+            this.lblSaude.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaude.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(245)))), ((int)(((byte)(230)))));
+            this.lblSaude.Location = new System.Drawing.Point(196, 402);
+            this.lblSaude.Name = "lblSaude";
+            this.lblSaude.Size = new System.Drawing.Size(15, 24);
+            this.lblSaude.TabIndex = 8;
+            this.lblSaude.Text = " ";
+            this.lblSaude.Click += new System.EventHandler(this.lblSaude_Click);
+            // 
+            // frmNome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(560, 479);
+            this.Controls.Add(this.lblSaude);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.lblIMC);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmNome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculo de IMC";
+            this.Load += new System.EventHandler(this.frmNome_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblIMC;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label lblSaude;
     }
 }
 
--- CalculadoraIMC/Form1.cs
+++ CalculadoraIMC/Form1.cs
@@ -10,11 +10,151 @@ using System.Windows.Forms;
 
 namespace CalculadoraIMC
 {
-    public partial class Form1 : Form
+    public partial class frmNome : Form
     {
-        public Form1()
+        public frmNome()
         {
             InitializeComponent();
+            
+}
+        private void frmNome_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(46, 139, 87);
+            
+        }
+
+        private void txtPeso_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if(txtPeso.Text == "")
+            {
+                //mostrar erro se o usuário não adicionar nada
+                MessageBox.Show("O valor do peso não pode estar vazio!","ERRO!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+
+                //mudar cor de fundo para deixar claro o erro do usuário
+                txtPeso.BackColor = Color.IndianRed;
+                txtPeso.ForeColor = Color.WhiteSmoke;
+
+                return;
+            }
+            else
+            {
+                //retornar ao normal se estiver certo
+                txtPeso.BackColor = Color.White;
+                txtPeso.ForeColor= Color.Black;
+            }
+            //no txtPeso_KeyPress, está impedindo a pessoa de colocar letras.
+
+            if (txtAltura.Text == "")
+            {
+                // mostrar erro se o usuário não adicionar nada
+                MessageBox.Show("O valor da altura não pode estar vazio!", "ERRO!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                //mudar cor de fundo para deixar claro o erro do usuário
+                txtAltura.BackColor = Color.IndianRed;
+                txtAltura.ForeColor = Color.WhiteSmoke;
+
+                return;
+            }
+            else
+            {
+                //retornar ao normal se estiver certo
+                txtAltura.BackColor = Color.White;
+                txtAltura.ForeColor = Color.Black;
+            }
+            //no txtAltura_KeyPress, está impedindo a pessoa de colocar letras.
+
+            
+            
+
+            //variaveis
+            double Peso = double.Parse(txtPeso.Text);
+            double Altura = double.Parse(txtAltura.Text);
+            double resultado = Peso / (Altura * Altura);
+
+            //mostrar resultado
+            txtResultado.Text = resultado.ToString();
+
+            if (resultado < 18.5)
+            {
+                lblSaude.ForeColor = Color.IndianRed;
+                lblSaude.BackColor = Color.White;
+                lblSaude.Text = ("Abaixo do peso");
+            }
+            else if (resultado >= 18.5 & resultado <= 24.9)
+            {
+                lblSaude.ForeColor = Color.Green;
+                lblSaude.BackColor = Color.White;
+                lblSaude.Text = ("Peso Saudável");
+            }
+            else if (resultado >=25 & resultado<=29.9)
+            {
+                lblSaude.ForeColor = Color.Yellow;
+                lblSaude.BackColor = Color.White;
+                lblSaude.Text = ("Sobrepeso");    
+            }
+            else if (resultado >=30 &  resultado <=34.9)
+            {
+                lblSaude.ForeColor = Color.IndianRed;
+                lblSaude.BackColor = Color.White;
+                lblSaude.Text = ("Obesidade Grau I");
+            }
+            else if(resultado >=35 &  resultado <=39.9)
+            {
+                lblSaude.ForeColor = Color.Red;
+                lblSaude.BackColor = Color.White;
+                lblSaude.Text = ("Obesidade Grau II");
+            }
+            else
+            {
+                lblSaude.ForeColor = Color.DarkRed;
+                lblSaude.BackColor = Color.White;
+                lblSaude.Text = ("Obesidade Grau III");
+            }
+        }
+
+        private void txtAltura_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //impedir a pessoa de colocar letras na altura
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != ',' && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtPeso_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //impedir a pessoa de colocar letras no peso
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != ',' && e.KeyChar != '\b')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtPeso_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //Pular pro próximo text box:
+                txtAltura.Focus();
+            }
+        }
+
+        private void txtAltura_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                //"pressionar" o botão de calcular com o enter.
+                btnCalcular.PerformClick();
+            }
+        }
+
+        private void lblSaude_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
